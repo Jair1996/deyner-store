efc269bb33f8261e4735d4e0be284802e65bce00
Jair Deyner <jairdeyner730791@gmail.com>
2023-02-01 09:09:50 -0500

    Product catalog and data fetching

--- src/app/public/catalog/catalog.module.ts
+++ src/app/public/catalog/catalog.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CatalogRoutingModule } from './catalog-routing.module';
 import { CatalogPageComponent } from './pages/catalog-page/catalog-page.component';
-
+import { SharedModule } from '../shared/shared.module';
+import { SharedModule as GlobalSharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    CatalogPageComponent
-  ],
-  imports: [
-    CommonModule,
-    CatalogRoutingModule
-  ]
+  declarations: [CatalogPageComponent],
+  imports: [CommonModule, CatalogRoutingModule, GlobalSharedModule, SharedModule],
 })
-export class CatalogModule { }
+export class CatalogModule {}
--- src/app/public/catalog/pages/catalog-page/catalog-page.component.ts
+++ src/app/public/catalog/pages/catalog-page/catalog-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/core/models/product.model';
+import { ProductService } from 'src/app/core/services/product.service';
 
 @Component({
   selector: 'app-catalog-page',
   templateUrl: './catalog-page.component.html',
-  styleUrls: ['./catalog-page.component.scss']
+  styleUrls: ['./catalog-page.component.scss'],
 })
-export class CatalogPageComponent {
+export class CatalogPageComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((products) => {
+      this.products = products;
+    });
+  }
 }
