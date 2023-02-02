328a9873371373e0e48206e138f9516fe3ee4f8b
Jair Deyner <jairdeyner730791@gmail.com>
2023-02-01 20:09:11 -0500

    detect role added

--- src/app/public/core/navbar/navbar.component.ts
+++ src/app/public/core/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { User } from 'src/app/core/models/user.model';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { CartService } from 'src/app/core/services/cart.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,19 +12,46 @@ import { CartService } from 'src/app/core/services/cart.service';
 })
 export class NavbarComponent implements OnInit {
   showMenu: boolean = true;
+  userLoggedIn: User | null = null;
 
   totalObservable!: Observable<number>;
 
-  constructor(private cartService: CartService, private authService: AuthService) {}
+  constructor(
+    private cartService: CartService,
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.totalObservable = this.cartService.getTotalProductsInCart;
     this.authService.authState().subscribe((data) => {
-      console.log(data);
+      if (data) {
+        const id = data.uid;
+
+        this.userService
+          .getUser(id)
+          .then((user) => {
+            const { name, lastname, role } = user.data() as User;
+            this.userLoggedIn = {
+              id,
+              name,
+              lastname,
+              role,
+            };
+          })
+          .catch(console.error);
+      } else {
+        this.userLoggedIn = null;
+      }
     });
   }
 
   toogleMenu() {
     this.showMenu = !this.showMenu;
+  }
+
+  logout() {
+    this.toogleMenu();
+    this.authService.logout();
   }
 }
